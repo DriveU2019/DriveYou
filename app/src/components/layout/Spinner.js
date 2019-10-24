5f23a53db09bbd8e98174e1c0abbe5367cb79e9c
juicejuz <dgeiss438@gmail.com>
2019-10-24 21:03:11 +0200

    Add Spinner Component

---
+++ app/src/components/layout/Spinner.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 [dage] @DriveYou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * --------------------------------------------------------------------------------
+ * Description:
+ *        TODO:
+ * --------------------------------------------------------------------------------
+ */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(10),
+    marginLeft: '45%'
+    // align: center
+  }
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <CircularProgress size={60} thickness={2} className={classes.progress} />
+    </div>
+  );
+}

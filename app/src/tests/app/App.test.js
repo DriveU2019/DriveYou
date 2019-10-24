c574b170b1969bdd8578b0fe8395c9533c51e366
juicejuz <dgeiss438@gmail.com>
2019-10-24 21:21:46 +0200

    First snap Tests

--- app/src/tests/app/App.test.js
+++ app/src/tests/app/App.test.js
@@ -23,7 +23,7 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../App';
+import App from '../../App';
 
 configure({ adapter: new Adapter() });
 

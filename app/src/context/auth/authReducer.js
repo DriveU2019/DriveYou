9c850097950b216358a26cd2b5d8ca19e9be1162
juicejuz <dgeiss438@gmail.com>
2019-11-01 21:04:09 +0100

    Add auth Reducer

--- app/src/context/auth/authReducer.js
+++ app/src/context/auth/authReducer.js
@@ -19,3 +19,39 @@
  *        TODO:
  * --------------------------------------------------------------------------------
  */
+
+import { USER_LOGIN, LOGIN_SUCCESS, LOGIN_FAIL, AUTH_ERROR } from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case USER_LOGIN:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};

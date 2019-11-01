9ddce86f16ce8bacf7b40b21f30348df0b7b593f
juicejuz <dgeiss438@gmail.com>
2019-11-01 21:43:17 +0100

    Add Generell SubSchemas

---
+++ api/models/subSchema/adressSchema.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 [dage] @DriveYou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * --------------------------------------------------------------------------------
+ * Description:
+ *        TODO:
+ * --------------------------------------------------------------------------------
+ */
+
+const mongoose = require('mongoose');
+const Joi = required('joi');
+
+const adressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    required: true
+  },
+  no: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true
+  },
+  postZip: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    required: true
+  },
+  city: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    required: true
+  },
+  state: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    required: true
+  },
+  country: {
+    type: String,
+    minlength: 2,
+    maxlength: 50,
+    required: true
+  }
+});
+
+exports.adressSchema = adressSchema;

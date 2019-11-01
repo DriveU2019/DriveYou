d1b64bb3636182d30da642164fc28b2c932b9a41
juicejuz <dgeiss438@gmail.com>
2019-11-01 21:04:31 +0100

    Add auth State

--- app/src/context/auth/AuthState.js
+++ app/src/context/auth/AuthState.js
@@ -15,7 +15,88 @@
 
 /*
  * --------------------------------------------------------------------------------
- * Description:
+ * Description: Handel Cases for Auth Stats
  *        TODO:
  * --------------------------------------------------------------------------------
  */
+
+import React from 'react';
+import axios from ' axios';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
+import { LOGIN_SUCCESS, USER_LOADED, AUTH_ERROR, LOGOUT } from '../types';
+
+const authState = state => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, setState] = React.useState();
+
+  //   Load User
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get('/api/v1/auth');
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR
+      });
+    }
+  };
+
+  //   Login User
+  const login = async formData => {
+    const config = {
+      headers: {
+        'content-Type': 'application/jason'
+      }
+    };
+    try {
+      const res = await axios.post('api/v1/auth', formData, config);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
+
+  //   Logout
+  const LOGOUT = () => dispatch({ type: LOGOUT });
+
+  //   Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+        loadUser,
+        login,
+        logout
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
